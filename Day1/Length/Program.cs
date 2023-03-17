dbf5bdc7d1c56fc72a6dc9cfacd051d87316538e
nabilaaini2510 <nabilaaini18@gmail.com>
2023-03-17 14:28:51 +0700

    Initial commit

---
+++ Day1/Length/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program{
+    static void Main(string[] args)
+    {
+        Console.Write("Input text:");
+        string name = Console.ReadLine();
+        int StringLength = name.Length;
+        Console.WriteLine(StringLength);
+    }
+    
+
+
+}
---
+++ Day1/ReverseWord/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program{
+    static void Main(string[] args)
+    {
+        Console.Write("Input text = ");
+        string name = Console.ReadLine();
+        string reverse = "";
+        for(int i = name.Length - 1; i >= 0; i--)
+        {
+            reverse += name[i];
+        }
+        Console.WriteLine("Reversed Word = " +reverse);
+        Console.WriteLine("Count Reversed Word = "+reverse.Length);
+        
+        // char[] charArray = name.ToCharArray();
+        // Array.Reverse(charArray);
+        // Console.WriteLine(charArray);
+    }
+}
