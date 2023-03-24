b0e5ff16c709d11e701bae1d79f81a749342345a
Nabila <nabilaaini18@gmail.com>
2023-03-24 11:04:28 +0700

    add day6_1 and day6_2

---
+++ Day6/Day6_1/CheckNumber.cs
@@ -0,0 +1,28 @@
+using System;
+namespace CheckingNumber{
+static void CheckNumber(){
+    Console.WriteLine("Input a number: ");
+        // for(int i = 0; i<=number; i++)
+        // {
+        //     if(i==0)
+        //     {
+        //         Console.Write(i +", ");
+        //     }
+        //     else if(i%3==0 && i%5==0)
+        //     {
+        //         Console.Write("FooBar, ");
+        //     }
+        //     else if(i%3==0)
+        //     {
+        //         Console.Write("Foo, ");
+        //     }
+        //     else if(i%5==0)
+        //     {
+        //         Console.Write("Bar, ");
+        //     }
+        //     else{
+        //         Console.Write(i +", ");
+        //     }
+        // }
+    }
+}
---
+++ Day6/Day6_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Calculator{
+    class Program{
+        delegate T CalculatorSum<T>(T a ,  T b);
+        static void Main(string[] args){
+            CalculatorSum<int> addnum = MethodsCal<int>.Sum;
+            Console.WriteLine(addnum(10, 20));
+            
+            CalculatorSum<int> multiplyNum = MethodsCal<int>.Multiply;
+            Console.WriteLine(multiplyNum(10, 20));
+            
+            CalculatorSum<int> substractNum = MethodsCal<int>.Substraction;
+            Console.WriteLine(substractNum(10, 20));
+            
+            // CalculatorSum<int> multiplyNum = new CalculatorSum<int>.MethodsCal.Multiply;
+            // Console.WriteLine(multiplyNum(10, 20));
+
+            // CalculatorSum<int> substarctNum = new CalculatorSum<int>.MethodsCal.Substraction;
+            // Console.WriteLine(substarctNum(10, 20));
+            // // CalculatorSum addNum =  new CalculatorSum(MethodsCal.Sum);
+            // Console.WriteLine(addNum(10, 20));
+
+            // CalculatorSum multiplyNum = new CalculatorSum(MethodsCal.Multiply);
+            // Console.WriteLine(multiplyNum(10, 20));   
+
+            // CalculatorSum substarctNum = new CalculatorSum(MethodsCal.Substraction);
+            // Console.WriteLine(substarctNum(10, 20));
+
+        }  
+    }
+}
