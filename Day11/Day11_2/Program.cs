3465107a5466821c057f3b60d0a3917119863412
Nabila <nabilaaini18@gmail.com>
2023-04-11 09:15:09 +0700

    add day 12

--- Day11/Day11_2/Program.cs
+++ Day11/Day11_2/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        string s = true.ToString(); // s = "True"
+        bool b = bool.Parse (s); // b = true
+        bool success = int.TryParse ("123", out int _);
+        Console.WriteLine(success); // True
+    }
+}
--- Day12/Day12_1/Program.cs
+++ Day12/Day12_1/Program.cs
@@ -5,14 +5,11 @@ class Program
     static void Main()
     {
         MyClass myClass = new MyClass(); // create a new instance of MyClass
-
         Console.WriteLine("Before garbage collection: " + GC.GetTotalMemory(false)); // print the amount of memory currently in use
-
         myClass = null; // set the reference to null, making the object eligible for garbage collection
-
         GC.Collect(); // explicitly invoke garbage collection
-
         Console.WriteLine("After garbage collection: " + GC.GetTotalMemory(false)); // print the amount of memory currently in use
+         // print the amount of memory currently in use
     }
 }
 
