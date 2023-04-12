baf32acb9483e53d4ef9e12c20d317f3a3602fd6
Nabila <nabilaaini18@gmail.com>
2023-04-12 09:16:04 +0700

    update debug

--- Day12/Day12_2/Program.cs
+++ Day12/Day12_2/Program.cs
@@ -19,7 +19,7 @@ class Text{
         }
         using (StreamWriter sw = File.CreateText(path))
         {
-           for(int i =0; i<= 10000; i++)
+           for(int i = 0; i<= 10; i++)
            {
                 sw.WriteLine(i+text);
            }
