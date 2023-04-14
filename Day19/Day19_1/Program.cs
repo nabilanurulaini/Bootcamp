07ad1891ff1a9e38c515669112d19179735d3e95
Nabila <nabilaaini18@gmail.com>
2023-04-14 10:21:21 +0700

    add day 19

---
+++ Day19/Day19_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program{
+    static async Task Main(string[] args){
+        Console.WriteLine("Main thread started");
+        await Task.Run(() => {
+            for(int i = 0; i < 10; i++){
+                Console.WriteLine("Task started");
+                Thread.Sleep(1000);
+                Console.WriteLine("Task ended");
+            }
+        });
+        Console.WriteLine("Main thread ended");
+    }
+}
---
+++ Day19/Day19_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static EventWaitHandle waitHandle = new EventWaitHandle(false, EventResetMode.ManualReset);
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Main thread waiting for signal form worker");
+        // menunggu antrian dari threadpool dan menjalankan worker thread
+        ThreadPool.QueueUserWorkItem(new WaitCallback(WorkerThread));
+        //nunggu 
+        waitHandle.WaitOne();
+        Console.WriteLine("Main thread received signal");
+    }
+
+    static void WorkerThread(object state)
+    {
+        Console.WriteLine("Worker thread started");
+        Thread.Sleep(2000);
+        waitHandle.Set();
+        Console.WriteLine("Worker thread signaled");
+    }
+}
