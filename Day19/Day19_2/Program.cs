c95a78b78dca3dbeef04f107a232945c89c6140c
Nabila <nabilaaini18@gmail.com>
2023-04-14 11:35:37 +0700

    add 19_3

--- Day19/Day19_2/Program.cs
+++ Day19/Day19_2/Program.cs
@@ -3,14 +3,14 @@ using System.Threading;
 
 class Program
 {
-    static EventWaitHandle waitHandle = new EventWaitHandle(false, EventResetMode.ManualReset);
+    static EventWaitHandle waitHandle = new EventWaitHandle(false, EventResetMode.AutoReset);
 
     static void Main(string[] args)
     {
         Console.WriteLine("Main thread waiting for signal form worker");
         // menunggu antrian dari threadpool dan menjalankan worker thread
-        ThreadPool.QueueUserWorkItem(new WaitCallback(WorkerThread));
-        //nunggu 
+        ThreadPool.QueueUserWorkItem(WorkerThread);
+       //  Task task1 = Task.Run(() => WorkerThread("Task 1"));
         waitHandle.WaitOne();
         Console.WriteLine("Main thread received signal");
     }
@@ -18,7 +18,7 @@ class Program
     static void WorkerThread(object state)
     {
         Console.WriteLine("Worker thread started");
-        Thread.Sleep(2000);
+
         waitHandle.Set();
         Console.WriteLine("Worker thread signaled");
     }
