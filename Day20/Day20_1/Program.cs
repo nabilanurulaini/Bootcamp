e92655eb6d50a9fc7343322741e9a279cbd592ff
nabila <nabilaaini18@gmail.com>
2023-04-16 22:41:17 +0700

    add day 20

---
+++ Day20/Day20_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Text;
+class ProgramFileStream
+{
+    static void Main(){
+        string path = @"C:\Users\HP\Bootcamp\Day20\Day20_1\TextFile1.txt";
+        //filemode open or create jika file tidak ada maka akan dibuatkan file baru
+        //fileaccess readwrite untuk membaca dan menulis
+        FileStream fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+        Console.WriteLine("Input your text here: ");
+        string text = Console.ReadLine();
+        byte[] data = Encoding.ASCII.GetBytes(text);
+        fs.Write(data, 0, data.Length);
+        fs.Close();
+    }
+
+   
+    
+}
