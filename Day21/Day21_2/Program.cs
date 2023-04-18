75021360939eb5e209b5a8618265d68b3fc01b44
Nabila <nabilaaini18@gmail.com>
2023-04-18 11:05:21 +0700

    add day 21

---
+++ Day21/Day21_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using MessagePack;
+
+[MessagePackObject]
+public class Person
+{
+	[Key(1)]
+	public string Name { get; set; }
+	[Key(2)]
+	public int Age { get; set; }
+	[Key(3)]
+	public int Height { get; set; }
+}
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		Person person = new Person
+        {
+            Name = "Johny",
+            Age = 30,
+            Height = 180
+        };
+        byte[] bytes = MessagePackSerializer.Serialize(person);
+        File.WriteAllBytes("person.bin", bytes);
+        //deserialize
+        byte[] bytesFromFile = File.ReadAllBytes("person.bin");
+        Person personDeserialized = MessagePackSerializer.Deserialize<Person>(bytesFromFile);
+        Console.Write("Name : "+ personDeserialized.Name + ", Age: "+ personDeserialized.Age + ", Height:  " + personDeserialized.Height);
+        
+	}
+}
---
+++ Day21/Day21_3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using ProtoBuf;
+[ProtoContract]
+public class Person
+{
+    [ProtoMember(1)]
+    public string Name { get; set; }
+    [ProtoMember(2)]
+    public int Age { get; set; }
+    [ProtoMember(3)]
+    public int Height { get; set; }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person
+        {
+            Name = "Johny",
+            Age = 30,
+            Height = 200
+        };
+        using (FileStream stream = new FileStream("person.bin", FileMode.Create))
+        {
+            Serializer.Serialize(stream, person);
+        }
+        //deserialize
+        using (FileStream stream = new FileStream("person.bin", FileMode.Open))
+        {
+            Person personDeserialized = Serializer.Deserialize<Person>(stream);
+            Console.Write("Name : " + personDeserialized.Name + ", Age: " + personDeserialized.Age + ", Height:  " + personDeserialized.Height);
+        }
+    }
+}
