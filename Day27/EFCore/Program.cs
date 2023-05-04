4fbdf90a2383b1929fa98b8e911ae71567b6c92b
nabila <nabilaaini18@gmail.com>
2023-05-04 19:34:43 +0700

    fix error on db generate models

---
+++ Day27/EFCore/Program.cs
@@ -0,0 +1,16 @@
+﻿
+namespace EFCore
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+            Northwind db = new Northwind();
+            //print Microsoft.EntityFrameworkCore.Sqlite ini tandanya jika database sudah connect
+            Console.WriteLine(db.Database.ProviderName);
+            //menampilkan semua data dari table categories
+            
+        }
+    }
+}
